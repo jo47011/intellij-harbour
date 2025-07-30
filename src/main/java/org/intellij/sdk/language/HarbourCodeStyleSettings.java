b075991d282bc87a192898bc99679a46c9562fef
developer <jo47011@gmail.com>
2025-07-30 14:43:30 +0200

    improve formatting

---
+++ src/main/java/org/intellij/sdk/language/HarbourCodeStyleSettings.java
@@ -0,0 +1,20 @@
+package org.intellij.sdk.language;
+
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.codeStyle.CustomCodeStyleSettings;
+
+/**
+ * Custom code style settings for Harbour language
+ */
+public class HarbourCodeStyleSettings extends CustomCodeStyleSettings {
+    
+    // Statement indentation settings (number of spaces)
+    public int LOCAL_INDENT = 0;    // Default: LOCAL at column 0
+    public int RETURN_INDENT = 0;   // Default: RETURN at column 0
+    public int DATA_INDENT = 0;     // Default: DATA at column 0
+    public int METHOD_INDENT = 0;   // Default: METHOD at column 0
+    
+    protected HarbourCodeStyleSettings(CodeStyleSettings container) {
+        super("HarbourCodeStyleSettings", container);
+    }
+}
